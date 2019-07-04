57e91cff5df681858aaafbcfd351da6b57415ed9
Milos Kapovic <milos@itis.ba>
2019-07-04 12:04:09 +0200

    Updates

---
+++ precache-manifest.5a54f06a1fdc14b86ce595e622cf5491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffb531693243fdaecb307465b92cc95",
+    "url": "/github/io/contactmanager/index.html"
+  },
+  {
+    "revision": "5c1c4d508048e4c373b2",
+    "url": "/github/io/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3fc38065705a572b88d3",
+    "url": "/github/io/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5c1c4d508048e4c373b2",
+    "url": "/github/io/contactmanager/static/js/2.1679b882.chunk.js"
+  },
+  {
+    "revision": "3fc38065705a572b88d3",
+    "url": "/github/io/contactmanager/static/js/main.3e64c4cc.chunk.js"
+  },
+  {
+    "revision": "446b7d1ff2c94cef4089",
+    "url": "/github/io/contactmanager/static/js/runtime~main.af5912b5.js"
+  }
+]);
